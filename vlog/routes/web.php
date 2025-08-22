17aeb8242af731aef6dd0a20a445b50cb6382cf7
Marco Esteban Rubio Gómez <themarco27@gmail.com>
2025-08-22 17:46:38 -0600

    First Class Example
    Navigation for screen and configuration bootstrap

--- vlog/routes/web.php
+++ vlog/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//ENDPOINT 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
