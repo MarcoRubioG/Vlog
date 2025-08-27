f2133fcee757b67d04aabab3ac5516f9957e3487
Marco Esteban Rubio Gómez <themarco27@gmail.com>
2025-08-27 15:46:36 -0600

    add initial blog pages (about, contact, post, welcome)

--- vlog/routes/web.php
+++ vlog/routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/post', function () {
+    return view('post');
+});
